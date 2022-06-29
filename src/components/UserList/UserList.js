7b52e80cc82d758601ab6a5d2ec1773f00b05ddb
Jonathas Veras <Jonathasveras42@gmail.com>
2022-06-28 21:16:22 -0300

    adição do map no UserList

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -13,21 +13,16 @@ export const UserList = () => {
     console.log(users);
   return (
     <ListGroup className='mt-4'>
-        <ListGroupItem className='d-flex'>
-            <strong>Usuário um</strong>
-            <div className='ml-auto'>
-                <Button className={style.buttonEdit} variant="warning"><Link to='/edit/1'>Editar</Link></Button>
-                <Button variant='danger'>Deletar</Button>
-            </div>
-        </ListGroupItem>
+        {users.map(user=>(
+            <ListGroupItem className='d-flex'>
+                <strong>{user.name}</strong>
+                <div className='ml-auto'>
+                    <Button className={style.buttonEdit} variant="warning"><Link to='/edit/1'>Editar</Link></Button>
+                    <Button variant='danger'>Deletar</Button>
+                </div>
+            </ListGroupItem>
+        ))}
 
-        <ListGroupItem className='d-flex'>
-            <strong>Usuário dois</strong>
-            <div className='ml-auto'>
-                <Button variant="warning" className={style.buttonEdit}><Link to='/edit/2'>Editar</Link></Button>
-                <Button variant='danger'>Deletar</Button>
-            </div>
-        </ListGroupItem>
     </ListGroup>
   )
 }
